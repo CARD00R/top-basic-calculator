1faae8f956411bb886cf58d25364a9442a257b91
CARD00R <39516150+CARD00R@users.noreply.github.com>
2024-04-05 17:17:53 +0400

    Add operator functions and input variables

---
+++ myscript.js
@@ -0,0 +1,18 @@
+
+//Input Variables
+num1 = 0;
+op = "";
+num2 = 0;
+
+// Operations
+function add (a,b) {return a+b}
+function subtract (a,b) {return a-b}
+function multiply (a,b) {return a*b}
+function divide (a,b) {return a/b}
+
+
+function operate(operator, number1, number2){
+    add(number1,number2);
+}
+
+console.log(multiply(1,2));
